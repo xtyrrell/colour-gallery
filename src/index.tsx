f2acdeb08fdf993f586dc5d1a06a14d6ff39df3b
Max Tyrrell <m@xtyrrell.com>
2023-06-01 23:06:38 +0200

    feat: adding colours on frontend

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-import { jsx } from 'hono/jsx'
+import { serveStatic } from "hono/cloudflare-workers";
 
-import { serveStatic } from 'hono/cloudflare-workers'
-
-import indexPageFn from './pages/index'
-import { getColours, Colour } from './services/colour-service'
+import indexPageFn, { IndexPage } from "./pages/index";
+import { getColours } from "./services/colour-service";
 
 type Bindings = {
-	DB: D1Database;
-}
+  DB: D1Database;
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
 // TODO: validation
 // https://hono.dev/concepts/stacks
@@ -19,60 +17,119 @@ const app = new Hono<{ Bindings: Bindings }>()
 // TODO:
 // app.basePath('/api')
 
-app.get('/', indexPageFn)
+app.get("/", indexPageFn);
 
-app.get('/colours', async c => {
-    console.log("GET /colours")
+app.get("/colours", async (c) => {
+  console.log("GET /colours");
 
-    const a = c.env.DB
+  const a = c.env.DB;
 
-    const colours = getColours(c);
-    
-    return c.json(
-colours
-      )
-})
+  const colours = getColours(c);
 
-app.post('/colours', async c => {
-  // Do something and return an HTTP response
-  const { hexCode, name } = await c.req.json()
+  return c.json(colours);
+});
 
-  if (!hexCode) return c.json({ success: false, error: "Missing hexCode value for new colour"}, 400)
-  if (!name) return c.json({ success: false, error: "Missing name value for new colour"}, 400)
+app.post("/colours", async (c) => {
+  // check type and respond differently
+  const t = c.req.headers.get("content-type");
+  console.log("t", t);
 
-  const { success } = await c.env.DB.prepare(`
+  // reference
+  // https://api.rubyonrails.org/classes/ActionController/MimeResponds.html#method-i-respond_to
+  // for how to make this better
+  if (t === "application/x-www-form-urlencoded") {
+    const { hexCode, name } = await c.req.parseBody();
+
+    if (!hexCode) return c.html(<p>Error: missing colour hex code</p>, 400);
+    if (!name) return c.html(<p>Error: missing colour name</p>, 400);
+
+    const { success, error, meta } = await c.env.DB.prepare(
+      `
     insert into colours (hex_code, name) values (?, ?)
-  `).bind(hexCode, name).run()
+  `
+    )
+      .bind(hexCode, name)
+      .run();
+
+    const colours = await getColours(c.env.DB);
+
+    if (success) {
+      // c.status(201);
+      return c.redirect('/');
+      // return c.html(
+      //   <IndexPage
+      //     colours={colours}
+      //     messages={["Successfully added your colour"]}
+      //     errors={[]}
+      //   />
+      // );
+    } else {
+      c.status(500);
+      return c.html(
+        <IndexPage
+          colours={colours}
+          messages={[]}
+          errors={[`Oops, could not add your colour. ${error}`]}
+        />
+      );    }
+  } else {
+  }
+
+  // Do something and return an HTTP response
+  const { hexCode, name } = await c.req.json();
+
+  if (!hexCode)
+    return c.json(
+      { success: false, error: "Missing hexCode value for new colour" },
+      400
+    );
+  if (!name)
+    return c.json(
+      { success: false, error: "Missing name value for new colour" },
+      400
+    );
+
+  const { success } = await c.env.DB.prepare(
+    `
+    insert into colours (hex_code, name) values (?, ?)
+  `
+  )
+    .bind(hexCode, name)
+    .run();
 
   if (success) {
-    c.status(201)
-    return c.json({ success: true })
+    c.status(201);
+    return c.json({ success: true });
   } else {
-    c.status(500)
-    return c.json({ success: false })
+    c.status(500);
+    return c.json({ success: false });
   }
-})
+});
 
-app.delete('/colours/:hexCode', async c => {
-// TODO: pull out the auth header value and throw if it's not
-// equal to some secret hardcoded 'password' value
-  const { hexCode } = c.req.param()
+app.delete("/colours/:hexCode", async (c) => {
+  // TODO: pull out the auth header value and throw if it's not
+  // equal to some secret hardcoded 'password' value
+  const { hexCode } = c.req.param();
 
-  const { success } = await c.env.DB.prepare(`
+  const { success } = await c.env.DB.prepare(
+    `
     delete from colours where hex_code = ?
-  `).bind(hexCode).run()
+  `
+  )
+    .bind(hexCode)
+    .run();
 
   if (success) {
-    c.status(200)
-    return c.json({ success: true })
+    c.status(200);
+    return c.json({ success: true });
   } else {
-    c.status(500)
-    return c.json({ success: false })
+    c.status(500);
+    return c.json({ success: false });
   }
-})
+});
 
 // app.get('/', serveStatic({ path: './index.html' }))
-app.get('/static/*', serveStatic({ root: './' }))
-app.get('/favicon.ico', serveStatic({ path: './favicon.ico' }))
+app.get("/static/*", serveStatic({ root: "./" }));
+app.get("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
 
-export default app
+export default app;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,79 @@
-import { Context } from "hono"
-import { getColours, Colour } from "../services/colour-service"
+import { Context } from "hono";
+import { getColours, Colour } from "../services/colour-service";
 
-const Layout = (props: { children?: string, colours: Colour[] }) => {
+const MODAL_ELEMENT_CLASS = "add-colour-modal";
+
+const AddColourModal = () => {
+  return (
+    <dialog class={MODAL_ELEMENT_CLASS}>
+      <form>
+        <button
+          type="submit"
+          aria-label="close"
+          formmethod="dialog"
+          formnovalidate
+        >
+          ✕
+        </button>
+      </form>
+      <form action="/colours" method="post">
+        <h2>Add a colour</h2>
+        <p>
+          <input type="color" id="hexCode" name="hexCode" />
+          <label for="hexCode">Colour</label>
+        </p>
+        <p>
+          <input type="text" id="name" name="name" />
+          <label for="name">Name</label>
+        </p>
+
+        <p>
+          <button type="submit" formmethod="post">
+            Submit
+          </button>
+        </p>
+      </form>
+    </dialog>
+  );
+};
+
+export const IndexPage = (props: { children?: string, colours: Colour[], messages: any[], errors: any[] }) => {
   return (
     <html>
       <head>
-      <title>Colour Gallery</title>
-      <meta charset="UTF-8" />
-      <link rel="stylesheet" href="static/resets.css" />
-      <link rel="stylesheet" href="static/styles.css" />
+        <title>Colour Gallery</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="static/resets.css" />
+        <link rel="stylesheet" href="static/styles.css" />
       </head>
 
-    <body>
-    <header>Colour Gallery</header>
+      <body>
+        <header>Colour Gallery</header>
+        <div>{props.messages?.map(m => <p>{m}</p>)}</div>
+        <div>{props.errors?.map(e => <p>{e}</p>)}</div>
 
-    <main class="colour-grid">
-      {props.colours.map(c => <div class="colour" style={`background: ${c.hexCode};`}></div>)}
-      <button class="colour create-colour">Add +</button>
-    </main>
+        <main class="colour-grid">
+          {props.colours.map((c) => (
+            <div class="colour" style={`background: ${c.hexCode};`}></div>
+          ))}
+          <button
+            class="colour create-colour"
+            onclick={`document.querySelector('.${MODAL_ELEMENT_CLASS}')?.showModal()`}
+          >
+            Add +
+          </button>
+        </main>
 
-    <div class="modal">
-			<input type="color">Choose your colour</input>
-		</div>
+        <AddColourModal />
 
-    <script src="src/index.js"></script>
-  </body>
-  </html>
-  )
-}
+        <script src="src/index.js"></script>
+      </body>
+    </html>
+  );
+};
 
 export default async function indexPageFn(c: Context) {
   const colours = await getColours(c.env.DB);
 
-  return c.html(<Layout colours={colours} />)
+  return c.html(<IndexPage colours={colours} />);
 }
